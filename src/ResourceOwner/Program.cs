3b1991b77eed5c8856ee740018011b6418263de6
Girish <girish@post.com>
2018-10-17 08:48:52 -0700

    Initial Project Structure

---
+++ src/ResourceOwner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResourceOwner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
